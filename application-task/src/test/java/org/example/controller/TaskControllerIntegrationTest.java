9753a627ac56e404496a9b4b05fd35326fe2c93c
AMKoryakin <koryakin1993@inbox.ru>
2025-03-12 06:43:10 +0300

    отладка интеграционных тестов

--- application-task/src/test/java/org/example/controller/TaskControllerIntegrationTest.java
+++ application-task/src/test/java/org/example/controller/TaskControllerIntegrationTest.java
@@ -9,21 +9,31 @@ import io.restassured.specification.RequestSpecification;
 import java.util.List;
 import org.example.dto.TaskDto;
 import org.example.entity.enums.TaskStatus;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.com.google.common.net.HttpHeaders;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.core.Is.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@EmbeddedKafka(
+        partitions = 1,
+        topics = {"tasks"},
+        brokerProperties = {
+                "listeners=PLAINTEXT://localhost:9092",
+                "log.dirs=/tmp/kafka-logs"
+        }
+)
 class TaskControllerIntegrationTest {
 
     private static final String TEST_URL = "/api/v1/tasks";
@@ -47,7 +57,7 @@ class TaskControllerIntegrationTest {
 
     @Test
     void findNonExistedTaskTest() {
-        getRequest(33, HttpStatus.NOT_FOUND.value());
+        getTaskById(33, HttpStatus.NOT_FOUND.value());
     }
 
     @Test
@@ -71,38 +81,18 @@ class TaskControllerIntegrationTest {
                              .description(description)
                              .build();
 
-        postRequest(taskDto, HttpStatus.OK.value());
+        createTask(taskDto, HttpStatus.OK.value());
 
-        var taskListResult = given(requestSpecification)
-                .when()
-                .get(TEST_URL)
-                .then()
-                .statusCode(HttpStatus.OK.value())
-                .body("size()", is(1))
-                .log()
-                .ifValidationFails(LogDetail.ALL)
-                .extract()
-                .body()
-                .as(new TypeRef<List<TaskDto>>() {})
-                .getLast();
+        var task = getLastTask();
+        var taskId = task.getId();
 
-        var taskId = taskListResult.getId();
+        validateNewTask(task, taskId, title, description);
 
-        assertThat(taskListResult.getId()).isEqualTo(taskId);
-        assertThat(taskListResult.getTitle()).isEqualTo(title);
-        assertThat(taskListResult.getDescription()).isEqualTo(description);
-        assertThat(taskListResult.getUserId()).isNull();
-        assertThat(taskListResult.getStatus()).isEqualTo(TaskStatus.NEW.toString());
+        task = getTaskById(taskId, HttpStatus.OK.value()).extract()
+                                                         .body()
+                                                         .as(TaskDto.class);
 
-        var taskResult = getRequest(taskId, HttpStatus.OK.value()).extract()
-                                                                  .body()
-                                                                  .as(TaskDto.class);
-
-        assertThat(taskResult.getId()).isEqualTo(taskId);
-        assertThat(taskResult.getTitle()).isEqualTo(title);
-        assertThat(taskResult.getDescription()).isEqualTo(description);
-        assertThat(taskResult.getUserId()).isNull();
-        assertThat(taskResult.getStatus()).isEqualTo(TaskStatus.NEW.toString());
+        validateNewTask(task, taskId, title, description);
     }
 
     @Test
@@ -116,22 +106,56 @@ class TaskControllerIntegrationTest {
                          .description(description)
                          .build();
 
-        given(requestSpecification)
-                .header("Content-type", "application/json")
-                .and()
-                .body(dto)
-                .when()
-                .put(TEST_URL + "/{id}", id)
-                .then()
-                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .log()
-                .ifValidationFails(LogDetail.ALL);
+        updateTask(id, dto, HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @Test
+    void updateTaskTest() {
+        var title = "Task Title for update";
+        var description = "Description for update";
+
+        var dto = TaskDto.builder()
+                         .title(title)
+                         .description(description)
+                         .build();
+
+        createTask(dto, HttpStatus.OK.value());
+
+        var task = getLastTask();
+        var taskId = task.getId();
+
+        var newTitle = "Task Title new";
+        var newDescription = "Description new";
+        var status = TaskStatus.IN_PROGRESS.toString();
+        var userId = 100L;
+        dto = TaskDto.builder()
+                     .id(taskId)
+                     .title(newTitle)
+                     .description(newDescription)
+                     .status(status)
+                     .userId(userId)
+                     .build();
+
+        updateTask(taskId, dto, HttpStatus.OK.value());
+
+
+        task = getTaskById(taskId, HttpStatus.OK.value()).extract()
+                                                                   .body()
+                                                                   .as(TaskDto.class);
+
+        validateTask(task, taskId, newTitle, newDescription, userId, status);
+
+        task = getLastTask();
+
+        validateTask(task, taskId, newTitle, newDescription, userId, status);
     }
 
     @Test
     void deleteUnExistedTaskTest() {
         var id = 3333;
-        getRequest(id, HttpStatus.NOT_FOUND.value());
+        getTaskById(id, HttpStatus.NOT_FOUND.value());
+
+        deleteTask(id, HttpStatus.OK.value());
 
         given(requestSpecification)
                 .when()
@@ -140,7 +164,7 @@ class TaskControllerIntegrationTest {
                 .statusCode(HttpStatus.OK.value());
     }
 
-    private ValidatableResponse postRequest(TaskDto dto, int statusCode) {
+    private ValidatableResponse createTask(TaskDto dto, int statusCode) {
         return given(requestSpecification)
                 .header("Content-type", "application/json")
                 .and()
@@ -153,7 +177,20 @@ class TaskControllerIntegrationTest {
                 .ifValidationFails(LogDetail.ALL);
     }
 
-    private ValidatableResponse getRequest(long id, int statusCode) {
+    private ValidatableResponse updateTask(long id, TaskDto dto, int statusCode) {
+        return given(requestSpecification)
+                .header("Content-type", "application/json")
+                .and()
+                .body(dto)
+                .when()
+                .put(TEST_URL + "/{id}", id)
+                .then()
+                .statusCode(statusCode)
+                .log()
+                .ifValidationFails(LogDetail.ALL);
+    }
+
+    private ValidatableResponse getTaskById(long id, int statusCode) {
         return given(requestSpecification)
                 .when()
                 .get(TEST_URL + "/" + id)
@@ -161,6 +198,48 @@ class TaskControllerIntegrationTest {
                 .statusCode(statusCode)
                 .log()
                 .ifValidationFails(LogDetail.ALL);
+    }
+
+    private List<TaskDto> getAllTasks(int size, int statusCode) {
+        return given(requestSpecification)
+                .when()
+                .get(TEST_URL)
+                .then()
+                .statusCode(statusCode)
+                .body("size()", greaterThanOrEqualTo(size))
+                .log()
+                .ifValidationFails(LogDetail.ALL)
+                .extract()
+                .body()
+                .as(new TypeRef<>() {});
+    }
+
+    private ValidatableResponse deleteTask(long id, int statusCode) {
+        return given(requestSpecification)
+                .when()
+                .delete(TEST_URL + "/" + id)
+                .then()
+                .statusCode(statusCode);
+    }
+
+    private TaskDto getLastTask() {
+        return getAllTasks(1, HttpStatus.OK.value()).getLast();
+    }
+
+    private void validateNewTask(TaskDto dto, long taskId, String title, String description) {
+        assertThat(dto.getId()).isEqualTo(taskId);
+        assertThat(dto.getTitle()).isEqualTo(title);
+        assertThat(dto.getDescription()).isEqualTo(description);
+        assertThat(dto.getUserId()).isNull();
+        assertThat(dto.getStatus()).isEqualTo(TaskStatus.NEW.toString());
+    }
+
+    private void validateTask(TaskDto dto, long taskId, String title, String description, long userId, String status) {
+        assertThat(dto.getId()).isEqualTo(taskId);
+        assertThat(dto.getTitle()).isEqualTo(title);
+        assertThat(dto.getDescription()).isEqualTo(description);
+        assertThat(dto.getUserId()).isEqualTo(userId);
+        assertThat(dto.getStatus()).isEqualTo(status);
     }
 
 }
